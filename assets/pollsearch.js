0b30e9bc7ad44db5704a8cca7deaf4c2f2518dab
BONAFIDE BOSS <amaanalmir@gmail.com>
2023-01-03 16:16:46 -0500

    update

--- assets/pollsearch.js
+++ assets/pollsearch.js
@@ -1,234 +1,203 @@
-const file = new XMLHttpRequest();
-file.open("GET", "pollData/allimdbpolls.json");
-file.send();
-file.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    var pollData = JSON.parse(file.responseText)
-    var authors = pollData.authors
-    var polls = pollData.polls
-    var authorData = polls.reduce(function (r, e) {
-      if (!r[e.authorid]) {
-        r[e.authorid] = {
-          author: "",
-          polls: 0,
-          votes: 0,
-          features: 0
-        }
-      }
-      r[e.authorid].author = e.author
-      r[e.authorid].polls += 1
-      r[e.authorid].votes += e.votes
-      var f = e.featured.toLowerCase() == 'yes' ? f = 1 : f = 0
-      r[e.authorid].features += f
-      return r
-    }, {})
+$('main').show()
+$('#search-query').focus()
 
-    var input;
-    var matches = location.hash.match(/#([^&]+)/i);
-    var hashFilter = matches && matches[1];
-    if (hashFilter) {
-      type = hashFilter.substring(0, 2).toLowerCase()
-      input = hashFilter.substring(2).replaceAll("+", " ")
-      input = input.replaceAll("%20", " ")
-      if (type == 'p=' & input.length > 0) {
-        $('#poll-search').val(input)
-        $('#poll-search-result').html('')
-        $('.data-loader.search-loader').show()
-        pollSearch(input);
-      } else if (type == 'a=' & input.length > 0) {
-        $('#item-filter').val('author')
-        $('#poll-search').val(input)
-        $('#poll-search-result').html('')
-        $('.data-loader.search-loader').show()
-        pollSearch(input);
-      }
+var searchOptions = $('form option').map(function () {
+  return $(this).val();
+}).get();
+
+$('#search-type').change(function () {
+  $('select:not(:eq(0))').attr('disabled', function (_, attr) {
+    return !attr
+  });
+})
+
+$('form').submit(function () {
+  const url = new URLSearchParams();
+  const searchType = $('#search-type').val()
+  url.set('query', $('#search-query').val())
+  url.set('search_type', searchType)
+  if (searchType != 'author') {
+    url.set('poll_type', $('#poll-type').val())
+    url.set('feature', $('#feature').val())
+    url.set('status', $('#status').val())
+    url.set('order', $('#order').val())
+  }
+  location.href = 'search?' + url
+})
+
+if (window.location.search) {
+  const url = new URLSearchParams(window.location.search);
+  var searchQuery = url.get('query')
+
+  if (searchQuery != null && searchQuery != "") {
+    searchQuery = searchQuery.split(" ")
+
+    var searchType = url.get('search_type')
+    var pollType = url.get('poll_type')
+    var feature = url.get('feature')
+    var status = url.get('status')
+    var order = url.get('order')
+
+    searchType = (searchType != null && searchType != "" && searchOptions.includes(searchType.toLowerCase())) ? searchType.toLowerCase() : 'poll'
+    pollType = (pollType != null && pollType != "" && searchOptions.includes(pollType.toLowerCase())) ? pollType.toLowerCase() : 'all'
+    feature = (feature != null && feature != "" && searchOptions.includes(feature.toLowerCase())) ? feature.toLowerCase() : 'all'
+    status = (status != null && status != "" && searchOptions.includes(status.toLowerCase())) ? status.toLowerCase() : 'all'
+    order = (order != null && order != "" && searchOptions.includes(order.toLowerCase())) ? order.toLowerCase() : 'latest'
+
+    $('#search-query').val(searchQuery.join(" "))
+    $('#search-type').val(searchType)
+    $('#poll-type').val(pollType)
+    $('#feature').val(feature)
+    $('#status').val(status)
+    $('#order').val(order)
+
+    if (searchType == 'author') {
+      $('select:not(:eq(0))').attr('disabled', true)
     }
 
-    $('form').on('submit', function () {
-      input = $('#poll-search').val()
-      if (!input.match(/^\s*$/)) {
-        $('#poll-search-result').html('')
-        $('.data-loader.search-loader').show()
-        var type = $('#item-filter').val() == 'author' ? type = '#a=' : type = '#p='
-        window.location = (type + input.replaceAll(' ', '+'));
-        pollSearch(input);
-      }
-    })
+    $('.data-loader.search-loader').show()
 
-    $('#item-filter').on('change', function () {
-      if (this.value == 'author') {
-        $('.search-type-title').html('Author Name:')
-        $('.search-filters select').attr('disabled', true)
-      } else {
-        $('.search-type-title').html('Poll Title:')
-        $('.search-filters select').attr('disabled', false)
-      }
-    })
+    const file = new XMLHttpRequest();
+    file.open("GET", "pollData/allimdbpolls.json");
+    file.send();
+    file.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        var pollData = JSON.parse(file.responseText)
+        var authors = pollData.authors
+        var polls = pollData.polls
 
-    function pollSearch(searchString) {
-      var searchheader = ''
-      var searchfooter = ''
-      var keywords = []
-      var searchresults = '';
-      var filteredlist = []
-      var imgurl = 'img/imdbpoll.png'
+        var authorData = polls.reduce((acc, x) => {
+          if (acc.find(y => y.authorid === x.authorid)) return acc;
+          const poll = polls.filter(y => y.authorid === x.authorid).length;
+          const votes = polls.filter(y => y.authorid === x.authorid).map(y => y.votes).reduce((a, b) => a + b, 0);
+          const features = polls.filter(y => y.authorid === x.authorid).map(y => y.featured).reduce((a, b) => a += b == 'Yes' ? 1 : 0, 0)
+          const avatar = authors.find(y => y.authorid == x.authorid).avatar
+          acc.push({
+            authorid: x.authorid,
+            author: x.author,
+            polls: poll,
+            votes: votes,
+            features: features,
+            avatar
+          })
+          return acc
+        }, []);
 
-      var polltype = $('#type-filter').val()
-      var orderby = $('#orderby-filter').val()
+        var avatar = 'img/imdbpoll.png'
+        var searchResult = ""
 
-      if (searchString.length > 0) {
-        keywords = searchString.toLowerCase().split(" ")
-        if ($('#item-filter').val() == 'poll') {
-          for (var i = 0; i < polls.length; i++) {
-            if (keywords.every(item => polls[i].title.toLowerCase().includes(item))) {
-
-              if (polltype == 'all') {
-                filteredlist.unshift(polls[i])
-              } else {
-                if (polls[i].type.toLowerCase() == polltype) {
-                  filteredlist.unshift(polls[i])
-                }
-              }
-
-              switch (orderby) {
-                case 'oldest':
-                  filteredlist.sort(function (a, b) {
-                    return new Date(a.date) - new Date(b.date);
-                  });
-                  break;
-                case 'votes':
-                  filteredlist.sort(function (a, b) {
-                    return b.votes - a.votes;
-                  });
-                  break;
-                case 'featured':
-                  filteredlist.sort(function (a, b) {
-                    var nameA = a.featured.toUpperCase();
-                    var nameB = b.featured.toUpperCase();
-                    if (nameA > nameB) {
-                      return -1;
-                    }
-                  });
-                  break;
-                default:
-                  filteredlist.sort(function (a, b) {
-                    return new Date(b.date) - new Date(a.date);
-                  });
-                  break;
-              }
-
+        if (searchType == 'poll') {
+          // Query Filter
+          polls = polls.filter(obj => {
+            if (searchQuery.every(word => obj['title'].toLowerCase().includes(word.toLowerCase()))) {
+              return obj
             }
+          })
+          // Type Filter
+          if (pollType != 'all') {
+            polls = polls.filter(obj => {
+              return obj['type'].toLowerCase() === pollType
+            })
           }
-          if (filteredlist.length != 0) {
-            for (var i = 0; i < filteredlist.length; i++) {
-              if (i < 1000) {
-                searchheader = '<h3>Search Results (' + (i + 1) + ')</h3><hr>'
-                searchfooter = 'Showing ' + (i + 1) + ' out of ' + filteredlist.length + ' results.'
-                var avatar = authors.find(entry => entry.authorid == filteredlist[i].authorid).avatar
-                if (avatar != "") {
-                  imgurl = avatar
-                }
-                var statusicon = '<i class="bi bi-circle-fill text-success"></i>'
-                if (filteredlist[i].status.toLowerCase() == "closed") {
-                  statusicon = '<i class="bi bi-circle-fill text-secondary"></i>'
-                } else if (filteredlist[i].status.toLowerCase() == "inactive" || filteredlist[i].status.toLowerCase() == "deleted") {
-                  statusicon = '<i class="bi bi-circle-fill text-danger"></i>'
-                }
-                var polldate = ""
-                var publishdate = ""
-                var featured = ""
-                if (filteredlist[i].date != "") {
-                  polldate = new Date(filteredlist[i].date).toDateString() + '<i class="bi bi-dot"></i>'
-                  publishdate = '<i class="bi bi-dot"></i> Published ' + timelapse(filteredlist[i].date)
-                }
-                if (filteredlist[i].featured.toLowerCase() == 'yes') {
-                  featured = '<i class="bi bi-dot"></i> Featured'
-                }
-                searchresults += '<div class="card mb-3">' +
-                  '<div class="card-body">' +
-                  '<div><img src="' + imgurl + '"><span>' +
-                  '<h5><a href="' + filteredlist[i].url + '">' + filteredlist[i].title + '</a></h5>' +
-                  '<p><a href="user?id=' + filteredlist[i].authorid + '">' + filteredlist[i].author + '</a><span class="text-muted">' + publishdate + '</span></p>' +
-                  '<p class="text-muted">' + polldate + filteredlist[i].type + ' Poll<i class="bi bi-dot"></i>' + filteredlist[i].status + '<i class="bi bi-dot"></i>' + filteredlist[i].votes.toLocaleString() + ' votes' + featured + '</p>' +
-                  '</span></div>' + statusicon + '</div></div><hr>'
+          // Featured Filter
+          if (feature != 'all') {
+            polls = polls.filter(obj => {
+              return obj['featured'].toLowerCase() === feature
+            })
+          }
+          // Status Filter
+          if (status != 'all') {
+            polls = polls.filter(obj => {
+              return obj['status'].toLowerCase() === status
+            })
+          }
+
+          switch (order) {
+            case 'oldest':
+              polls.sort(function (a, b) {
+                return new Date(a.date) - new Date(b.date);
+              });
+              break;
+            case 'votes':
+              polls.sort(function (a, b) {
+                return b.votes - a.votes;
+              });
+              break;
+            default:
+              polls.sort(function (a, b) {
+                return new Date(b.date) - new Date(a.date);
+              });
+              break;
+          }
+
+          searchResult += '<h3>Search Results (' + polls.length + ')</h3><hr>'
+          if (polls.length != 0) {
+            polls.forEach(i => {
+              var authorAvatar = authors.find(entry => entry.authorid == i.authorid).avatar
+              if (authorAvatar != "") {
+                avatar = authorAvatar
               }
-            }
+
+              var statusicon = '<i class="bi bi-circle-fill text-success"></i>'
+              if (i.status.toLowerCase() == "closed") {
+                statusicon = '<i class="bi bi-circle-fill text-secondary"></i>'
+              } else if (i.status.toLowerCase() == "inactive" || i.status.toLowerCase() == "deleted") {
+                statusicon = '<i class="bi bi-circle-fill text-danger"></i>'
+              }
+
+              var polldate = ""
+              var publishdate = ""
+              var featured = ""
+              if (i.date != "") {
+                polldate = new Date(i.date).toDateString() + '<i class="bi bi-dot"></i>'
+                publishdate = '<i class="bi bi-dot"></i> Published ' + timelapse(i.date)
+              }
+              if (i.featured.toLowerCase() == 'yes') {
+                featured = '<i class="bi bi-dot"></i> Featured'
+              }
+
+              searchResult += '<div class="card mb-3">' +
+                '<div class="card-body">' +
+                '<div><img src="' + avatar + '"><span>' +
+                '<h5><a href="' + i.url + '">' + i.title + '</a></h5>' +
+                '<p><a href="user?id=' + i.authorid + '">' + i.author + '</a><span class="text-muted">' + publishdate + '</span></p>' +
+                '<p class="text-muted">' + polldate + i.type + ' Poll<i class="bi bi-dot"></i>' + i.status + '<i class="bi bi-dot"></i>' + i.votes.toLocaleString() + ' votes' + featured + '</p>' +
+                '</span></div>' + statusicon + '</div></div><hr>'
+            });
           } else {
-            searchheader = '<h3>Search Results (0)</h3><hr>'
-            searchresults = 'No poll found.'
+            searchResult += '<p class="fs-5 d-grid gap-2 fw-bold text-center py-5"><i class="fs-1 bi bi-emoji-frown"></i> No polls found!</p><hr>'
           }
-        } else if ($('#item-filter').val() == 'author') {
-          var authorCount = 0
-          for (const i in authorData) {
-            if (Object.hasOwnProperty.call(authorData, i)) {
-              const element = authorData[i];
-              if (element.author.toLowerCase().includes(searchString.toLowerCase())) {
-                authorCount = authorCount + 1
-                searchheader = '<h3>Search Results (' + (authorCount) + ')</h3><hr>'
-                searchfooter = 'Showing ' + (authorCount) + ' out of ' + authors.length + ' authors.'
-                var avatar = authors.find(entry => entry.authorid == i).avatar
-                if (avatar != "") {
-                  imgurl = avatar
-                }
-                searchresults += '<div class="card mb-3">' +
-                  '<div class="card-body"><div>' +
-                  '<img src="' + imgurl + '"><span>' +
-                  '<span><h5><a href="user?id=' + i + '">' + element.author + '</a></h5>' +
-                  '<p class="text-secondary">Polls: ' + element.polls + '<i class="bi bi-dot"></i>Votes: ' + element.votes.toLocaleString() + '<i class="bi bi-dot"></i>Features: ' + element.features + '</p></span>' +
-                  '</div></div></div><hr>'
-              }
-            }
-          }
+          searchResult += 'Showing ' + polls.length + ' out of ' + polls.length + ' results.'
         }
-        setTimeout(() => {
-          $('.data-loader.search-loader').hide()
-          $('#poll-search-result').html(searchheader + searchresults + searchfooter)
-          if ($('#poll-search-result').html() == 0) {
-            $('#poll-search-result').html('No author found.')
+
+        if (searchType == 'author') {
+          authorData = authorData.filter(obj => {
+            if (searchQuery.every(word => obj['author'].toLowerCase().includes(word.toLowerCase()))) {
+              return obj
+            }
+          })
+
+          searchResult += '<h3>Search Results (' + authorData.length + ')</h3><hr>'
+          if (authorData.length != 0) {
+            authorData.forEach(i => {
+              searchResult += '<div class="card mb-3">' +
+                '<div class="card-body"><div>' +
+                '<img src="' + i.avatar + '"><span>' +
+                '<span><h5><a href="user?id=' + i.authorid + '">' + i.author + '</a></h5>' +
+                '<p class="text-secondary">Polls: ' + i.polls + '<i class="bi bi-dot"></i>Votes: ' + i.votes.toLocaleString() + '<i class="bi bi-dot"></i>Features: ' + i.features + '</p></span>' +
+                '</div></div></div><hr>'
+            })
+          } else {
+            searchResult += '<p class="fs-5 d-grid gap-2 fw-bold text-center py-5"><i class="fs-1 bi bi-emoji-frown"></i> No authors found!</p><hr>'
           }
-        }, 1000);
+          searchResult += 'Showing ' + authorData.length + ' out of ' + authorData.length + ' results.'
+        }
 
-      } else {}
+        $('#search-result').html(searchResult)
+        $('.data-loader.search-loader').hide()
+        $('#search-result').show()
+      }
+
     }
-
-    $('.data-loader.loader-one').hide()
-    $('main').show()
-    $('#poll-search').focus()
-
   }
-}
-
-function timelapse(date) {
-  var currentDateTime = new Date()
-  var difference = Math.abs(currentDateTime - new Date(date))
-
-  var mm = difference;
-  var sec = Math.ceil(difference / (1000))
-  var min = Math.ceil(difference / (1000 * 60))
-  var hr = Math.round(difference / (1000 * 60 * 60))
-  var day = Math.round(difference / (1000 * 60 * 60 * 24))
-  var month = Math.round(difference / (1000 * 60 * 60 * 24 * 30))
-  var year = Math.round(difference / (1000 * 60 * 60 * 24 * 30 * 12))
-
-  difference = mm + ' milliseconds ago';
-  if (sec >= 1 && sec < 60) {
-    var text = (sec > 1) ? ' seconds ago' : ' second ago'
-    difference = sec + text;
-  } else if (min >= 1 && min < 60) {
-    var text = (min > 1) ? ' minutes ago' : ' minute ago'
-    difference = min + text;
-  } else if (hr >= 1 && hr < 24) {
-    var text = (hr > 1) ? ' hours ago' : ' hour ago'
-    difference = hr + text
-  } else if (day >= 1 && day < 30) {
-    var text = (day > 1) ? ' days ago' : ' day ago'
-    difference = day + text
-  } else if (month >= 1 && month < 12) {
-    var text = (month > 1) ? ' months ago' : ' month ago'
-    difference = month + text
-  } else if (year >= 1) {
-    var text = (year > 1) ? ' years ago' : ' year ago'
-    difference = year + text
-  }
-  return difference
 }
